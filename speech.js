c405aafa893b41008ec5ebfc8e1d36e2332e2f9f
Cloudiu9 <claudiubordea5@gmail.com>
2023-11-25 16:25:53 +0200

    Fixing small typos.

--- speech.js
+++ speech.js
@@ -1,27 +1,25 @@
-document.addEventListener("touchstart", on_touch)
-document.addEventListener("mousedown", on_touch)
-var recognition = new webkitSpeechRecognition()
-recognition.lang = 'en-US'
-function on_touch()
-{
-    if(recognition.start)
-    {
-        recognition.start()
-        recognition.started = true
+document.addEventListener("touchstart", on_touch);
+document.addEventListener("mousedown", on_touch);
+
+var recognition = new webkitSpeechRecognition();
+recognition.lang = 'en-US';
+
+function on_touch() {
+    if (recognition.start) {
+        recognition.start();
+        recognition.started = true;
     }
 }
 
-function onend()
-{
-    recognition.stop()
-    recognition_started = false
+function onend() {
+    recognition.stop();
+    recognition.started = false;
 }
-recognition.onend = onend
-recognition.onsounded = onend
-recognition.onspeechend = onend
-recognition.onresult = on_results
 
-function on_results(e)
-{
-    document.getElementById("text".innerHTML += "You said:  " + e.results[0][0].transcript + ", accuracy: "+e.results[0][0].confidence + "<br>")
+recognition.onend = onend;
+recognition.onspeechend = onend;
+recognition.onresult = on_results;
+
+function on_results(e) {
+    document.getElementById("text").innerHTML += "You said:  " + e.results[0][0].transcript + ", accuracy: " + e.results[0][0].confidence + "<br>";
 }
